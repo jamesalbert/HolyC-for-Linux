786c9a32de91581fc0a0dde7f8c894b15d934dfb
jamesalbert <jimmychanga.cheese@gmail.com>
2018-01-06 21:48:13 -0800

    more pypi setup

--- setup.py
+++ setup.py
@@ -1,15 +1,63 @@
-from distutils.core import setup
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from codecs import open  # To use a consistent encoding
+from os import path
 
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-  name = 'secularize',
-  packages = ['secularize'],
-  version = '0.0.1',
-  description = 'run HolyC source on linux...and secularify wasn\'t as catchy',
-  author = 'James Albert',
-  author_email = 'jamesrobertalbert@gmail.com',
-  url = 'https://www.github.com/jamesalbert/HolyC-for-Linux',
-  download_url = 'https://github.com/jamesalbert/HolyC-for-Linux/archive/v0.0.1.tar.gz',
-  keywords = ['holyc', 'linux', 'terry', 'lord', 'temple', 'mlg'],
-  classifiers = [],
+    name='secularize',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # http://packaging.python.org/en/latest/tutorial.html#version
+    version='0.0.1',
+
+    description='run HolyC source on linux',
+    long_description=long_description,  #this is the
+
+    # The project's main homepage.
+    url='https://www.github.com/jamesalbert/HolyC-for-Linux',
+
+    # Author details
+    author='jamesalbert',
+    author_email='jamesrobertalbert@gmail.com',
+
+    # Choose your license
+    license='MIT',
+
+    entry_points={
+       'console_scripts': [
+           'secularize = bin.secularize:main',
+       ],
+    },
+
+    # See https://PyPI.python.org/PyPI?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    # What does your project relate to?
+    keywords='holyc linux terry lord temple mlg',
+
+    packages=["secularize", "bin", "config"],
+
 )
